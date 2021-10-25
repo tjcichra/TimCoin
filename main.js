7683e70471b423e172cabefdbcf6af13a0c9cab4
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-10-24 23:14:53 -0400

    created basic Blockchain class with genesis block

--- main.js
+++ main.js
@@ -14,3 +14,14 @@ class Block {
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 }
+
+class Blockchain {
+
+    constructor() {
+        this.chain = [this.createGenesisBlock()];
+    }
+
+    createGenesisBlock() {
+        return new Block(0, '10/24/2021', 'Genesis block', '0');
+    }
+}
