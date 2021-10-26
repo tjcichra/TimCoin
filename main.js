edaf05cfcc98b858f8c2cc834a35854d01f7caeb
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-10-25 22:09:15 -0400

    added blockchain validation

--- main.js
+++ main.js
@@ -34,10 +34,37 @@ class Blockchain {
         newBlock.hash = newBlock.calculateHash();
         this.chain.push(newBlock);
     }
+
+    isChainValid() {
+        for(let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 let timCoin = new Blockchain();
 timCoin.addBlock(new Block(1, '02/24/2021', { amount: 4}));
 timCoin.addBlock(new Block(2, '03/13/2021', { amount: 10}));
+
+console.log('Is blockchain valid? ' + timCoin.isChainValid());
+
+//Tamper with the blockchain
+timCoin.chain[1].data = { amount: 100 };
+//Try to be clever and recalculate the hash ahead of time
+timCoin.chain[1].hash = timCoin.chain[1].calculateHash();
+
+//Still not valid! The previous hash of the next block is now incorrect
+console.log('Is blockchain valid? ' + timCoin.isChainValid());
 
 console.log(JSON.stringify(timCoin, null, 4));
