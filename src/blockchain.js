94f6897854a60b0bfb92296cfcfbe8d89d8e2077
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-10-27 22:37:59 -0400

    added transaction signing

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,4 +1,6 @@
 const SHA256 = require('crypto-js/sha256');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
 class Transaction {
 
@@ -6,6 +8,31 @@ class Transaction {
         this.fromAddress = fromAddress;
         this.toAddress = toAddress;
         this.amount = amount;
+    }
+
+    calculateHash() {
+        return SHA256(this.fromAddress + this.toAddress + this.amount).toString();
+    }
+
+    signTransaction(signingKey) {
+        if(signingKey.getPublic('hex') !== this.fromAddress) {
+            throw new Error('You cannot sign transaction for other wallets!');
+        }
+
+        const hashTx = this.calculateHash();
+        const sig = signingKey.sign(hashTx, 'base64');
+        this.signature = sig.toDER('hex');
+    }
+
+    isValid() {
+        if(this.fromAddress === null) return true;
+
+        if(!this.signature || this.signature.length === 0) {
+            throw new Error('No signature in this transaction');
+        }
+
+        const publicKey = ec.keyFromPublic(this.fromAddress, 'hex');
+        return publicKey.verify(this.calculateHash(), this.signature);
     }
 }
 
@@ -31,6 +58,16 @@ class Block {
 
         console.log("Block mined: " + this.hash)
     }
+
+    hasValidTransactions() {
+        for(const tx of this.transactions) {
+            if(!tx.isValid()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
 
 class Blockchain {
@@ -52,7 +89,7 @@ class Blockchain {
 
     minePendingTransactions(miningRewardAddress) {
         let block = new Block(Date.now(), this.pendingTransactions);
-        block.previousHash = this.chain[this.chain.length - 1];
+        block.previousHash = this.chain[this.chain.length - 1].hash;
         block.calculateHash();
         block.mineBlock(this.difficulty);
 
@@ -63,7 +100,15 @@ class Blockchain {
         ]
     }
 
-    createTransaction(transaction) {
+    addTransaction(transaction) {
+        if(!transaction.fromAddress || !transaction.toAddress) {
+            throw new Error('Transaction must include from and to address');
+        }
+
+        if(!transaction.isValid()) {
+            throw new Error('Cannot add invalid transaction to chain');
+        }
+
         this.pendingTransactions.push(transaction);
     }
 
@@ -89,6 +134,10 @@ class Blockchain {
         for(let i = 1; i < this.chain.length; i++) {
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
+
+            if(!currentBlock.hasValidTransactions()) {
+                return false;
+            }
 
             if(currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
--- src/main.js
+++ src/main.js
@@ -1,18 +1,33 @@
 const { Blockchain, Transaction } = require('./blockchain')
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
+
+const myKey = ec.keyFromPrivate('1ce2bfcac550901793186ad8199721594f2441d8ebd669933987bbd8f9683ac4');
+const myWalletAddress = myKey.getPublic('hex');
 
 let timCoin = new Blockchain();
 
-timCoin.createTransaction(new Transaction('address1', 'address2', 100));
-timCoin.createTransaction(new Transaction('address2', 'address1', 50));
+const tx1 = new Transaction(myWalletAddress, 'justin', 10);
+tx1.signTransaction(myKey);
+timCoin.addTransaction(tx1);
+
 
 console.log('Starting the miner.');
-timCoin.minePendingTransactions('tims-address');
+timCoin.minePendingTransactions(myWalletAddress);
 
 console.log();
-console.log('Balance of tim is', timCoin.getBalanceOfAddress('tims-address'));
+console.log('Balance of tim is', timCoin.getBalanceOfAddress(myWalletAddress));
 
 console.log('Starting the miner again.');
-timCoin.minePendingTransactions('tims-address');
+timCoin.minePendingTransactions(myWalletAddress);
 
 console.log();
-console.log('Balance of tim is', timCoin.getBalanceOfAddress('tims-address'));
+console.log('Balance of tim is', timCoin.getBalanceOfAddress(myWalletAddress));
+
+console.log('Is chain valid?', timCoin.isChainValid());
+console.log();
+
+console.log('');
+timCoin.chain[1].transactions[0].amount = 1;
+
+console.log('Is chain valid?', timCoin.isChainValid());
